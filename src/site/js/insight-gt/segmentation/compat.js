f89efa31fbd1a5b777fe130d73f081ede92f4978
Ricardo <jricardobrenes@gmail.com>
2018-11-26 11:57:08 -0600

    Fixing linter

--- src/site/js/insight-gt/segmentation/compat.js
+++ src/site/js/insight-gt/segmentation/compat.js
@@ -3,11 +3,11 @@
  * Copyright 2015  Kota Yamaguchi
  */
 define(function() {
-  return {
-      // Internet Explorer doesn't support ImageData().
-      createImageData: function (width, height) {
-          var context = document.createElement("canvas").getContext("2d");
-          return context.createImageData(width, height);
-      }
-  };
+    return {
+        // Internet Explorer doesn't support ImageData().
+        createImageData : function(width, height) {
+            var context = document.createElement('canvas').getContext('2d');
+            return context.createImageData(width, height);
+        },
+    };
 });
--- src/site/js/insight-gt/segmentation/segmentation.js
+++ src/site/js/insight-gt/segmentation/segmentation.js
@@ -8,26 +8,27 @@
  *
  * Copyright 2015  Kota Yamaguchi
  */
-define(["segmentation/pff",
-        "segmentation/slic",
-        "segmentation/slico",
-        "segmentation/watershed"],
-function (pff, slic, slico, watershed) {
+define([
+    'segmentation/pff',
+    'segmentation/slic',
+    'segmentation/slico',
+    'segmentation/watershed',
+], function(pff, slic, slico, watershed) {
+    var methods = {
+        pff : pff,
+        slic : slic,
+        slico : slico,
+        watershed : watershed,
+    };
 
-  var methods = {
-    pff: pff,
-    slic: slic,
-    slico: slico,
-    watershed: watershed
-  };
+    methods.create = function(imageData, options) {
+        options = options || {};
+        options.method = options.method || 'slico';
+        if (!methods[options.method]) {
+            throw 'Invalid method: ' + options.method;
+        }
+        return new methods[options.method](imageData, options);
+    };
 
-  methods.create = function (imageData, options) {
-    options = options || {};
-    options.method = options.method || "slico";
-    if (!methods[options.method])
-      throw "Invalid method: " + options.method;
-    return new methods[options.method](imageData, options);
-  };
-
-  return methods;
+    return methods;
 });

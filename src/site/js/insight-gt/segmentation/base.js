551d2f408b513965faacb8e056eb6086253bd4b8
Ricardo <jricardobrenes@gmail.com>
2018-11-26 10:14:10 -0600

    Fixing linter

--- src/site/js/insight-gt/segmentation/base.js
+++ src/site/js/insight-gt/segmentation/base.js
@@ -3,22 +3,16 @@
  *
  * Copyright 2015  Kota Yamaguchi
  */
-define(["segmentation/compat"],
-function (compat) {
-  
-    function BaseSegmentation(imageData, options) {
-    if (!(imageData instanceof ImageData))
-      throw "Invalid ImageData";
-    
-    var context = document.createElement("canvas").getContext("2d");
-    this.imageData = context.createImageData(imageData.width,  imageData.height);
-    this.imageData.data.set(imageData.data);
+define(function() {
+    function BaseSegmentation(imageData) {
+        var context = document.createElement('canvas').getContext('2d');
+        this.imageData = context.createImageData(imageData.width, imageData.height);
+        this.imageData.data.set(imageData.data);
     }
 
+    BaseSegmentation.prototype.finer = function() {};
 
-  BaseSegmentation.prototype.finer = function () {};
+    BaseSegmentation.prototype.coarser = function() {};
 
-  BaseSegmentation.prototype.coarser = function () {};
-
-  return BaseSegmentation;
+    return BaseSegmentation;
 });
